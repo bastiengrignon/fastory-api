cc25837406a64833f0b309a76048cef712a1d1e9
bastiengrignon <basgrignon@gmail.com>
2021-10-07 01:05:53 +0200

    Add service and result from request

--- src/controller/search/search.controller.ts
+++ src/controller/search/search.controller.ts
@@ -1,8 +1,23 @@
 import { Request, Response } from 'express'
+import {SwapiService} from '../../service/swapi'
 
 
 export class SearchController {
-    public static search(req: Request, res: Response): void {
-        res.json("test").end()
+    public static async search(req: Request, res: Response): Promise<void> {
+        const { keyword } = req.query
+        if (!keyword) {
+            res.status(400).json("No url provided").end()
+            return
+        }
+
+        const peoples = await SwapiService.searchPeople(keyword.toString())
+        const planets = await SwapiService.searchPlanets(keyword.toString())
+        const films = await SwapiService.searchFilms(keyword.toString())
+        const species = await SwapiService.searchSpecies(keyword.toString())
+        const vehicles = await SwapiService.searchVehicles(keyword.toString())
+        const starships = await SwapiService.searchStarships(keyword.toString())
+        const results = [...peoples, ...planets, ...films, ...species, ...vehicles, ...starships]
+
+        res.json(results).end()
     }
 }
---
+++ src/service/swapi/swapi.service.ts
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+export class SwapiService {
+    static baseUrl = "https://swapi.dev/api"
+
+    public static async searchPeople(string: string): Promise<any[]> {
+        return await axios.get(`${ this.baseUrl }/people/?search=${ string }`).then(r => r.data['results'])
+    }
+
+    public static async searchPlanets(string: string): Promise<[]> {
+        return await axios.get(`${ this.baseUrl }/planets/?search=${ string }`).then(r => r.data['results'])
+    }
+
+    public static async searchFilms(string: string): Promise<[]> {
+        return await axios.get(`${ this.baseUrl }/films/?search=${ string }`).then(r => r.data['results'])
+    }
+
+    public static async searchSpecies(string: string): Promise<[]> {
+        return await axios.get(`${ this.baseUrl }/species/?search=${ string }`).then(r => r.data['results'])
+    }
+
+    public static async searchVehicles(string: string): Promise<[]> {
+        return await axios.get(`${ this.baseUrl }/vehicles/?search=${ string }`).then(r => r.data['results'])
+    }
+
+    public static async searchStarships(string: string): Promise<[]> {
+        return await axios.get(`${ this.baseUrl }/starships/?search=${ string }`).then(r => r.data['results'])
+    }
+}
