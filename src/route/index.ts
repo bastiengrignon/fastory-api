ef36284d5d164ba85ddc6cc866d36220b19c5cce
bastiengrignon <basgrignon@gmail.com>
2021-10-08 13:17:24 +0200

    Add new route

--- src/route/index.ts
+++ src/route/index.ts
@@ -3,6 +3,7 @@ import { SearchController } from '../controller/search'
 
 const Routes = (app: Application) => {
     app.get('/', SearchController.search)
+    app.get('/:type/:id', SearchController.getType)
 }
 
 export default Routes
