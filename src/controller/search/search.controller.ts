d741ef18f287f4a456dcae8f82b18706a0b74321
bastiengrignon <basgrignon@gmail.com>
2021-10-08 13:18:13 +0200

    Add new controller method

--- src/controller/search/search.controller.ts
+++ src/controller/search/search.controller.ts
@@ -3,6 +3,17 @@ import {SwapiService} from '../../service/swapi'
 
 
 export class SearchController {
+    public static async getType(req: Request, res: Response): Promise<void> {
+        const { type, id } = req.params
+        if (!type && !id) {
+            res.status(404).json("Type or id not found").end()
+            return
+        }
+
+        const typeReturned = await SwapiService.getObject(type, parseInt(id))
+        res.json(typeReturned).end()
+    }
+
     public static async search(req: Request, res: Response): Promise<void> {
         const { keyword } = req.query
         if (!keyword) {
